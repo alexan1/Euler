1e17842ecdb08e9b73022fc270a815018a8eb5c8
alexan1 <alexan1@gmail.com>
2014-06-10 20:36:39 -0400

    first

---
+++ Euler/Euler.fs
@@ -0,0 +1,15 @@
+﻿namespace Euler
+
+module Euler =
+    
+    open System
+
+    let fibonacci() = Seq.unfold (fun state ->
+        if (fst state + snd state > Int32.MaxValue) then None
+        else Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)
+
+    let fibonacci1 = Seq.unfold (fun state ->
+        if (fst state + snd state > Int32.MaxValue) then None
+        else Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)
+
+    let fibonacciBig() = Seq.unfold (fun state ->Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)
---
+++ Euler/Script.fsx
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Euler.fs"
+open Euler
+
+let res = fibonacci() |> Seq.nth 10 
+
+let res1 = fibonacci1 |> Seq.nth 10 
+
+// Define your library scripting code here
+
