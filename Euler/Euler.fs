d08c44dd0d2778c68eddb10b31d6946eb74efb48
alexan1 <alexan1@gmail.com>
2014-06-14 16:51:42 -0400

    again

--- Euler/Euler.fs
+++ Euler/Euler.fs
@@ -4,6 +4,14 @@ module Euler =
     
     open System
 
+    //check if even
+    let isEven x =
+        x % 2 = 0
+
+    //check if even bigint
+    let isEvenI x =
+        x % 2I = 0I
+    
     //generating Fibonacci sequence
     let fibonacci() = Seq.unfold (fun state ->
         if (fst state + snd state > Int32.MaxValue) then None
@@ -12,6 +20,8 @@ module Euler =
     //generating bigint Fibonacci sequence
     let fibonacciI() = Seq.unfold (fun state -> Some(fst state + snd state, (snd state, fst state + snd state))) (0I,1I)
 
+    //another way to generate Fibonacci number
+    //TODO: check how it works
     let fibonacciN (n : int) =
         let x1 = (1.0 + sqrt 5.0) /2.0
         let x2 = (1.0 - sqrt 5.0) / 2.0
@@ -31,3 +41,24 @@ module Euler =
         | _ when (n = 0I) -> 1I
         | _ when (n = 1I) -> 1I
         | _ -> n * factorialI(n-1I)
+
+    //get larget prime factor for int
+    let largestPrimeFactor x = 
+        let rec largestPrimeFactorRec x y =
+            if x = y then y
+            elif x % y = 0 then largestPrimeFactorRec (x / y) y
+            else largestPrimeFactorRec x (y + 1)
+        largestPrimeFactorRec x 2
+    
+    //get larget prime factor for bigint number
+    let largestPrimeFactorI x = 
+        let rec largestPrimeFactorRecI x y =
+            if x = y then y
+            elif x % y = 0I then largestPrimeFactorRecI (x / y) y
+            else largestPrimeFactorRecI x (y + 1I)
+        largestPrimeFactorRecI x 2I
+    
+    //check if papamert is Palindrome
+    // generic x can be int or string
+    let isPalindrome x =
+    x.ToString() = new string(Array.rev (x.ToString().ToCharArray()))
