6e5b93eadaa3863ba000f9f0df45e9a2b901ffce
alexan1 <alexan1@gmail.com>
2014-06-14 15:26:23 -0400

    second

--- Euler/Euler.fs
+++ Euler/Euler.fs
@@ -4,12 +4,30 @@ module Euler =
     
     open System
 
+    //generating Fibonacci sequence
     let fibonacci() = Seq.unfold (fun state ->
         if (fst state + snd state > Int32.MaxValue) then None
-        else Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)
+        else Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)    
 
-    let fibonacci1 = Seq.unfold (fun state ->
-        if (fst state + snd state > Int32.MaxValue) then None
-        else Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)
+    //generating bigint Fibonacci sequence
+    let fibonacciI() = Seq.unfold (fun state -> Some(fst state + snd state, (snd state, fst state + snd state))) (0I,1I)
 
-    let fibonacciBig() = Seq.unfold (fun state ->Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)
+    let fibonacciN (n : int) =
+        let x1 = (1.0 + sqrt 5.0) /2.0
+        let x2 = (1.0 - sqrt 5.0) / 2.0
+        int ((x1**(float (n - 1)) - x2**(float (n - 1))) / sqrt 5.0)
+
+    let fibonacciM() = Seq.initInfinite(fun x -> fibonacciN x)
+
+    //factoral function
+    let rec factorial n =
+        match n with
+        | 0 | 1 -> 1
+        | _ -> n * factorial(n-1)
+
+    //factorial bigint
+    let rec factorialI (n : bigint) =
+        match n with
+        | _ when (n = 0I) -> 1I
+        | _ when (n = 1I) -> 1I
+        | _ -> n * factorialI(n-1I)
--- Euler/Script.fsx
+++ Euler/Script.fsx
@@ -4,9 +4,18 @@
 #load "Euler.fs"
 open Euler
 
-let res = fibonacci() |> Seq.nth 10 
+let res = Euler.fibonacci() |> Seq.nth 9
 
-let res1 = fibonacci1 |> Seq.nth 10 
+let res1 = Euler.fibonacci() |> Seq.take 10 |> List.ofSeq 
 
-// Define your library scripting code here
+let Bigres = Euler.fibonacciI() |> Seq.nth 10
+
+let fac = Euler.factorial 10
+
+let facI = Euler.factorialI 10I
+
+let fibN = Euler.fibonacciN 4
+
+let fibM = Euler.fibonacciM() |> Seq.take 10 |> List.ofSeq 
+
 
